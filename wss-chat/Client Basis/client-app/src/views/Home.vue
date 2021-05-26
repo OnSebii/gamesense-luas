20f9684d2e1a2a24e0eb5f1112e979f72b81e1ea
OnSebii <lang.sebastian.006@gmail.com>
2021-05-26 15:00:40 +0200

    WSS

---
+++ wss-chat/Client Basis/client-app/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h4 v-if="message != null" class="altert alert-danger text-center">
+      {{ message }}
+    </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ws: null,
+      message: null,
+    };
+  },
+  created() {
+    this.ws = new WebSocket('ws://localhost:3000');
+    this.ws.onmessage = ({ data }) => {
+      this.message = data;
+    };
+  },
+};
+</script>
