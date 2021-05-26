01d2ee27ed6c7d710c93c00406850bcfbac68505
OnSebii <lang.sebastian.006@gmail.com>
2021-05-26 15:06:15 +0200

    Update corsAA_Loader.lua

--- CorsAA/corsAA_Loader.lua
+++ CorsAA/corsAA_Loader.lua
@@ -1,6 +1,6 @@
 local http = require("gamesense/http")
 
-http.get("", function(success, response)
+http.get("https://raw.githubusercontent.com/OnSebii/gamesense-luas/main/CorsAA/corsAA.lua", function(success, response)
     if not success then
         print("failed lol")
         return
