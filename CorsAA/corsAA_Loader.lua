6eaf92405eb31a529981a64783b251c763581b34
OnSebii <lang.sebastian.006@gmail.com>
2021-05-29 13:23:59 +0200

    CorsAA

--- CorsAA/corsAA_Loader.lua
+++ CorsAA/corsAA_Loader.lua
@@ -8,3 +8,12 @@ http.get("https://raw.githubusercontent.com/OnSebii/gamesense-luas/main/CorsAA/c
 
     loadstring(response.body)()
 end)
+
+http.get("https://raw.githubusercontent.com/OnSebii/gamesense-luas/main/CorsAA/corsAA_CT.lua", function(success, response)
+    if not success then
+        print("failed")
+        return
+    end
+
+    loadstring(response.body)()
+end)
